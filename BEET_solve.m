abe13b1dada98dca6a8422ea84a012bee58abf00
jonathanjadams <168124964+jonathanjadams@users.noreply.github.com>
2024-04-30 17:39:23 -0400

    Add files via upload

--- BEET_solve.m
+++ BEET_solve.m
@@ -8,22 +8,33 @@
 % BEET_solve: this program solves a model with behavioral expectations or
 % sentiment shocks
 
-% Version 0.1 (2023/10/16)
+% Version 0.2 (2024/4/30)
 
-% dependencies: Uhlig Toolkit subroutines (add to path), BEET_foreterm (optional)
+% dependencies: Uhlig Toolkit subroutines (add to path), GENSYS functions (optional, add to path), BEET_foreterm (optional)
 
 % inputs: 
-% - FIRE model expressed in Uhlig Toolkit form, with matrices
-%   (AA_fire, BB_fire, CC_fire ... LL_fire, etc.)
-% - vectors fcast_vars, senti_endovars, senti_exovars, which determine which
-%   endogenous variables should have forecasts included in the model, which
-%   endogenous variables should have sentiment shocks, and which exogenous
-%   states should have sentiment shocks
-% - (optional) additional cumulative horizons fcast_hors
+%  -  **AA_fire**, **BB_fire**,... **NN_fire** (*Required*): matrices which encode the rational expectations model in "Uhlig form" (corresponding to matrices $A,B,...N$ respectively in Uhlig's notation)
+%  - **BE_phivec**: vector of coefficients $[\phi_0,\phi_1,...,\phi_J]$ that encode a deterministic behaiovral expectations operator $\mathbb{E}^k$ in terms of current and past rational forecasts:
+%    $$\mathbb{E}^k_t[x_{t+1}] = \sum_{j=0}^{J}\phi_j \mathbb{E}^k_{t-j}[x_{t+1}]$$
+%  - **senti_exovars**: vector of indices identifying the exogenous variables about which agents have stochastic belief distortions or "sentiments"
+%  - **senti_endovars**: vector of indices identifying the endogenous variables about which agents have stochastic belief distortions or "sentiments"
+%  - **fcast_vars**: vector of indices identifying the endogenous variables whose one-period-ahead forecasts should be added to the model as additional endogenous variables.  This will be done automatically for variables identified in **senti_endovars**, even if they are not specified here.
+%  - **fcast_hors**: matrix identifying variables whose many-periods-ahead forecasts should be added to the model as additional endogenous variables.  If there are $n$ such forecasts to be added, **fcast_hors** is specified as an $n\times 3$ matrix.  In each row, the first entry indexes the endogenous variable to be forecast, the second entry indexes the forecast horizon (an integer number of periods), and the third entry is set to $1$ if the forecast is cumulative and $0$ otherwise.
+%  - **BEET_method**: scalar identifying which method should be used to solve the model.  $0$ indicates Uhlig (the default), $1$ indicates GENSYS (*still experimental*), $2$ indicates both (for diagnostic purposes)
 
-
+n_exo_fire = size(NN_fire,1); %number of exogenous states in the FIRE base model (sometimes different than n_exo if expectations are behavioral)
+n_fl = size(GG_fire,1);  %number of forward looking equations in base model
+if ~exist('CC_fire','var')
+    CC_fire = [];
+    AA_fire = zeros(0,n_fl);
+    BB_fire = AA_fire;
+    DD_fire = zeros(0,n_exo_fire);
+    JJ_fire = zeros(n_fl,0);
+    KK_fire = zeros(n_fl,0);
+end
+n_con = size(CC_fire,1); %number of contemporaneous equations in base model
 if ~exist('BE_phivec','var') %if no behavioral expectation is specified, set expanded L,M,N matrices to be FIRE matrices
-    DD_firebig = DD_fire;
+     DD_firebig = DD_fire;
     LL_firebig = LL_fire;
     MM_firebig = MM_fire;
     NN_firebig = NN_fire;
@@ -40,21 +51,7 @@ else
     NN_misp = NN_firebig;
     NN_misp(1:size(NN_fire,1),:) = kron(BE_phivec,NN_fire);
 end
-
-
-n_fl = size(GG_fire,1);  %number of forward looking equations in base model
 n_exo = size(NN_firebig,1); %number of exogenous states in the (modified) base model
-n_exo_fire = size(NN_fire,1); %number of exogenous states in the FIRE base model (sometimes different than n_exo if expectations are behavioral)
-
-if ~exist('CC_fire','var')
-    CC_fire = [];
-    AA_fire = zeros(0,n_fl);
-    BB_fire = AA_fire;
-    DD_fire = zeros(0,n_exo);
-    JJ_fire = zeros(n_fl,0);
-    KK_fire = zeros(n_fl,0);
-end
-n_con = size(CC_fire,1); %number of contemporaneous equations in base model
 
 if ~exist('senti_endovars','var')
     senti_endovars = [];
@@ -186,9 +183,52 @@ NN_plm(1:n_exo,1+n_exo:end)= NN_state_senti;
 %solve!
 NN = NN_plm;
 
-warnings = [];
-options;
-solve;
+
+%Which method should be used: Uhlig (0) or GENSYS (1) or both (2)?
+if ~exist('BEET_method','var')
+    BEET_method = 0; %default: set it to zero (Uhlig)
+end
+
+if BEET_method ~= 1  %calls the Uhlig toolkit
+    warnings = [];
+    options;
+    solve; 
+end
+if BEET_method == 1  ||  BEET_method == 2  %calls GENSYS
+    if ~exist('AA_fire','var')
+        di('To convert to GENSYS form, the Uhlig form must be specified without a "y" endogenous variable, i.e. there should be no A, B, C, or D matrix.')
+    end
+    if ~exist('senti_endovars','var')
+        di('Sentiment shocks are not yet compatable with the GENSYS solution.');
+    end
+    if ~exist('senti_exovars','var')
+        di('Sentiment shocks are not yet compatable with the GENSYS solution.');
+    end
+
+    %To make consistent, need to add a variable for the behavioral expectation,
+    %then additional variables for the current and past rational expectations
+    BE_g0 = [-GG -FF; eye(size(GG)) zeros(size(FF))];
+    BE_g1 = [HH zeros(size(HH)); zeros(size(HH)) eye(size(HH))];
+    BE_c = zeros(2*size(HH,1),1);
+    BE_psi = [LL*NN_plm + MM; zeros(size(LL*NN_plm + MM))];
+    BE_pi = [zeros(size(HH)); eye(size(HH))];
+
+    [G1,C,impact,fmat,fwt,ywt,gev,eu,loose] = gensys(BE_g0,BE_g1,BE_c,BE_psi,BE_pi);
+    
+    %Map to Uhlig parameters:
+    PP_gensys = G1;
+    forwardmat = 0;
+    for jj=1:100
+        forwardmat = ywt*fmat^(jj-1)*fwt*NN_plm^jj + forwardmat;
+    end
+    forwardmat=real(forwardmat);
+    QQ_gensys = impact+forwardmat;
+end
+if BEET_method == 1 %if you solved only with GENSYS, save implied Uhlig matrices for post-processing
+    PP=PP_gensys;
+    QQ=QQ_gensys;
+end
+
 
 %I return NN to match the *actual* law of motion 
 %(for other post-processing functions)
